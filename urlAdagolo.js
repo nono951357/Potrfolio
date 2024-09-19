da4a92eab7619f67cda6792a089919e17b838456
Berki Levente Márkó <berkileventemarko@gmail.com>
2024-09-19 11:30:07 +0200

    Frissites Fix

--- urlAdagolo.js
+++ urlAdagolo.js
@@ -1,5 +1,6 @@
     const oraiProjektekUrls = [
     { url: 'https://nono951357.github.io/Frontend_Gyak/', isDeployed: true },
+    { url: 'https://github.com/nono951357/ScoreBoard', isDeployed: false },
     ];
 
     const sajatProjektekUrls = [
@@ -13,21 +14,24 @@
     const oraiProjektekBox = document.querySelector('.column:nth-child(1) .project-list');
     const sajatProjektekBox = document.querySelector('.column:nth-child(2) .project-list');
     const haziFeladatBox = document.querySelector('.column:nth-child(3) .project-list');
-    
+
 oraiProjektekUrls.forEach(project => {
-    const projectName = project.url.split('/')[3];
+    const splitIndex = project.isDeployed ? 3 : 4;
+    const projectName = project.url.split('/')[splitIndex];
     const status = project.isDeployed ? 'Deployed' : 'Uploaded';
     oraiProjektekBox.innerHTML += `<li><a href="${project.url}" target="_blank">${projectName}</a> - ${status}</li>\n`;
-    });
-    
+});
+
 sajatProjektekUrls.forEach(project => {
-    const projectName = project.url.split('/')[3];
+    const splitIndex = project.isDeployed ? 3 : 4;
+    const projectName = project.url.split('/')[splitIndex];
     const status = project.isDeployed ? 'Deployed' : 'Uploaded';
     sajatProjektekBox.innerHTML += `<li><a href="${project.url}" target="_blank">${projectName}</a> - ${status}</li>\n`;
-    });
-    
+});
+
 haziFeladatUrls.forEach(project => {
-    const projectName = project.url.split('/')[3];
+    const splitIndex = project.isDeployed ? 3 : 4;
+    const projectName = project.url.split('/')[splitIndex];
     const status = project.isDeployed ? 'Deployed' : 'Uploaded';
     haziFeladatBox.innerHTML += `<li><a href="${project.url}" target="_blank">${projectName}</a> - ${status}</li>\n`;
-    });
+});
